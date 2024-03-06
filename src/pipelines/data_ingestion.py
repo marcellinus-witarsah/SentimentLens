d9e3b47e326989e71256f8585b2b4f745fdc9556
Marcellinus Aditya Witarsah <aw.marcel@gmail.com>
2024-03-06 20:25:59 +0700

    add stage name and log messaged

--- src/pipelines/data_ingestion.py
+++ src/pipelines/data_ingestion.py
@@ -8,9 +8,12 @@ from src.utils.common import logger
 from src.config.configuration import ConfigurationManager
 from src.data.data_ingestion import DataIngestion
 
+STAGE_NAME = "Data ingestion Stage"
 try:
+    logger.info(f">>>>>> {STAGE_NAME} started <<<<<<")
     configuration_manager = ConfigurationManager()
     data_ingestion = DataIngestion(config=configuration_manager.get_data_ingestion_config())
     data_ingestion.extract_zip_file()
+    logger.info(f">>>>>> {STAGE_NAME} completed <<<<<<")
 except Exception as e:
     logger.error(e)
