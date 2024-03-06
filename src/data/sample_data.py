dd330ca1d5fd4224adae21aca238586886bad6fd
Marcellinus Aditya Witarsah <aw.marcel@gmail.com>
2024-03-06 19:48:23 +0700

    add lofs and ensuring annotations on the function

--- src/data/sample_data.py
+++ src/data/sample_data.py
@@ -1,30 +1,30 @@
-# -*- coding: utf-8 -*-
 import os
 import sys
 import click
-import logging
 import pandas as pd
 from pathlib import Path
 from dotenv import find_dotenv, load_dotenv
+from ensure import ensure_annotations
 
 load_dotenv(find_dotenv())
 sys.path.append(os.getenv("PROJECT_FOLDER"))
 from src.utils.common import logger
-from src.utils.common import parse
 from src.utils.common import get_data_frame
 
 
 @click.command()
 @click.argument("input_folder", type=click.Path(exists=True))
 @click.argument("output_filepath", type=click.Path())
-def main(input_folder, output_filepath):
-    """Runs data processing scripts to turn raw data from (../raw) into
-    cleaned data ready to be analyzed (saved in ../processed).
+@ensure_annotations
+def main(input_folder: str, output_filepath: str) -> None:
+    """
+    Perform data sampling
     """
     df = pd.DataFrame({})
     for path in Path(input_folder).glob("*.gz"):
         logger.info(f"Load {path} ...")
         temp = get_data_frame(path)  # load .gz type data
+        logger.info(f"Append to main dataframe ...")
         df = pd.concat([df, temp])
 
     logger.info(f"Sample data")
