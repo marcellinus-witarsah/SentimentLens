86461422a1c39927f480761a5e98ef5c6bedb919
Marcellinus Aditya Witarsah <aw.marcel@gmail.com>
2024-03-10 22:31:11 +0700

    built flask web app for serving the sentiment classifier model

---
+++ app.py
@@ -0,0 +1,25 @@
+from flask import Flask, render_template, request
+from src.pipelines.model_predictor import ModelPredictoripeline
+
+app = Flask(__name__)
+
+
+@app.route("/", methods=["GET"])
+def hello():
+    return render_template("index.html")
+
+
+@app.route("/", methods=["POST", "GET"])
+def predict():
+    result = None
+    if request.method == "POST":
+        model_predictor_pipeline = ModelPredictoripeline()
+        data = request.form["review"]
+        result = (
+            "Positive" if model_predictor_pipeline.run(data=data) == 1 else "Negative"
+        )
+    return render_template("index.html", data=data, result=result)
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8080, debug=True)
